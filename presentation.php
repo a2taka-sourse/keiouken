38bf981dab14ce7c21640f77a071c3960f1da75d
_a2taka_ <101450250+a2taka-sourse@users.noreply.github.com>
2023-06-28 19:59:55 +0900

    add newfile instead of too many changes

--- presentation.php
+++ presentation.php
@@ -11,19 +11,12 @@
 </head>
 
 <body>
-  <img width="1000" height="800"
-    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmytown.mizuho-re.co.jp%2Fmz-abenobashi-s%2Fwp-content%2Fuploads%2F2016%2F08%2F200191_11-04Tennoji.jpg&f=1&nofb=1&ipt=26e3b0b08cca9e8b6aed14c0a8e31893daf06ccf74397402c331059fe417bb26&ipo=images"
-    alt="我が母校" title="野人厩舎">
-  <h1>↑の情報研究部、文展筐体候補掲示板</h1>
-  <p>
-  <h2>投票用のYoutube動画付き紹介です。</h2>
-  </p>
-  <p>
-  <h2>※このサイトは特にスマホで見にくいよ</h2>
-  </p>
-  <p>
-  <h2>※インターネット黎明期のサイトと思って我慢してね</h2>
-  </p>
+  <img width="1000" height="800" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmytown.mizuho-re.co.jp%2Fmz-abenobashi-s%2Fwp-content%2Fuploads%2F2016%2F08%2F200191_11-04Tennoji.jpg&f=1&nofb=1&ipt=26e3b0b08cca9e8b6aed14c0a8e31893daf06ccf74397402c331059fe417bb26&ipo=images"
+      alt="我が母校" title="野人厩舎">
+  <h1>↑(野人の舎)の情報研究部、文展筐体候補掲示板</h1>
+  <p><h2>投票用のYoutube動画付き紹介です。</h2></p>
+  <p><h2>※このサイトは特にスマホで見にくいよ</h2></p>
+  <p><h2>※インターネット黎明期のサイトと思って我慢してね</h2></p>
   <div class="box2">
     <h2>ポン</h2>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/5Cge1bVB9h0"
@@ -92,14 +85,14 @@
 
   </div>
   <div class="box2">
-    <h1>音ゲー</h1>
-    <h2>チュウニズム</h2>
+    <h2>音ゲー</h2>
+    <h3>チュウニズム</h3>
     <img width="600" height="400"
       src="https://www.taito.co.jp/Content/images/zone/1/item/201407/1ae1de2a-5bde-4f01-b11f-25d605df5898_p_01_ja.jpg"
       alt="海の写真" title="空と海">
-    <h2>太鼓の達人</h2>
+    <h3>太鼓の達人</h3>
     <img width="600" height="400" src="https://th.bing.com/th/id/OIP.TNRasjNuSA48sbLI4WnIlwAAAA?pid=ImgDet&rs=1">
-    <h2>ポップンミュージック</h2>
+    <h3>ポップンミュージック</h3>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/ALvhaGRXVjY" title="[ポップン] MEGALOVANIA EX"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -107,18 +100,18 @@
 
   </div>
   <div class="box2">
-    <h1>トレカゲーム</h1>
-    <h2>オレカバトル</h2>
+    <h2>トレカゲーム</h2>
+    <h3>オレカバトル</h3>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/q7yUsPGwEv4"
       title="【オレカバトル】オレーカ今までありがとう！＃shorts" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen></iframe>
-    <h2>ヒーローバンク</h2>
+    <h3>ヒーローバンク</h3>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/20rIQscTeG4" title="ヒーローバンク アーケード「遊び方ムービー」"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen></iframe>
-    <h2>ドラゴンボールヒーローズ</h2>
+    <h3>ドラゴンボールヒーローズ</h3>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/DTD5yjctFac" title="ドラゴンボールヒーローズ初プレイ♪"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -140,16 +133,13 @@
 
   </div>
   <div class="box2">
-    <br>
     <h2>クレーンゲーム</h2>
     <img width="600" height="400" src="https://th.bing.com/th/id/OIP.H55ROgeGvkFj-M9QpENtTwAAAA?pid=ImgDet&rs=1">
-  </div>
-  <div class="box2">
     <h2>ハッピーボタン</h2>
     <iframe width="600" height="400" src="https://www.youtube.com/embed/wzRBebhmno4" title="ハッピーボタン3" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen></iframe></br>
-  </div>
+      allowfullscreen></iframe>
+
   </div>
   <div id="pin">
 
@@ -157,106 +147,79 @@
   <div class="box3">
     <h1>アーケードゲーム候補の提案</h1>
     <p>
-    <h2>
-      1.作りたいゲームがあるなら、検索ボックスにそのゲームの名前を入れて検索
-    </h2>
+      <h2>
+        1.作りたいゲームがあるなら、検索ボックスにそのゲームの名前を入れて検索
+      </h2>
     </p>
     <p>
-    <h2>
-      2.そのゲームのプレイ動画を見つけたら、selectを押す。
-      もし、見つけられなかったなら、キーワードを指定しなおすか、諦めてください。
-    </h2>
+      <h2>
+        2.そのゲームのプレイ動画を見つけたら、selectを押す。
+        　もし、見つけられなかったなら、キーワードを指定しなおすか、諦めてください。
+      </h2>
     </p>
-    <p>
-    <h1>↓検索ボックス</h1>
-    </p>
-    <div id="search-container">
-      <input id="query" type="text" placeholder="提案したいゲームは?">
-      <button id="search-button" type="button">Search</button>
-    </div>
-    <div id="results"></div>
-    <script>
-      // APIキーを設定する
-      var apiKey = "AIzaSyCqjWTidDp1_AkkoXr1Rh9APmV7ayRWGGU";
-      var quary;
+    <p><h1>↓検索ボックス</h1></p>
+        <div id="search-container">
+          <input id="query" type="text" placeholder="提案したいゲームは?">
+          <button id="search-button" type="button">Search</button>
+        </div>
+        <div id="results"></div>
+        <script>
+             // APIキーを設定する
+            var apiKey = "AIzaSyCqjWTidDp1_AkkoXr1Rh9APmV7ayRWGGU";
 
-      // 検索ボタンがクリックされたら、YouTube Data APIにリクエストを送る
-      $("#search-button").click(function () {
+            // 検索ボタンがクリックされたら、YouTube Data APIにリクエストを送る
+            $("#search-button").click(function () {
 
-        // 入力されたキーワードを取得する
-        query = $("#query").val();
-        //var squery = new XMLHttpRequest();
-
-        //squary.open('POST', "///c%3A/Users/086-g7710309-72/Downloads/presentation.html");
-        //squary.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-        //squary.send(quary);
-        // リクエストのパラメーターを設定する
-        var params = {
-          part: "snippet",
-          q: query,
-          type: "video",
-          maxResults: 10,
-          key: apiKey
-        };
-        // GETメソッドでリクエストを送る
-        $.get("https://www.googleapis.com/youtube/v3/search", params, function (data) {
-          // レスポンスから動画の情報を取得する
-          var items = data.items;
-          // 結果を表示する要素を空にする
-          $("#results").empty();
-          // 各動画について、タイトルとサムネイルと選択ボタンを表示する
-          for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            var title = item.snippet.title;
-            var thumbnail = item.snippet.thumbnails.default.url;
-            var videoId = item.id.videoId;
-            var html = `
+              // 入力されたキーワードを取得する
+              var query = $("#query").val();
+              // リクエストのパラメーターを設定する
+              var params = {
+                part: "snippet",
+                q: query,
+                type: "video",
+                maxResults: 10,
+                key: apiKey
+              };
+              // GETメソッドでリクエストを送る
+              $.get("https://www.googleapis.com/youtube/v3/search", params, function (data) {
+                // レスポンスから動画の情報を取得する
+                var items = data.items;
+                // 結果を表示する要素を空にする
+                $("#results").empty();
+                // 各動画について、タイトルとサムネイルと選択ボタンを表示する
+                for (var i = 0; i < items.length; i++) {
+                  var item = items[i];
+                  var title = item.snippet.title;
+                  var thumbnail = item.snippet.thumbnails.default.url;
+                  var videoId = item.id.videoId;
+                  var html = `
                              <div class="item">
                                <h3>${title}</h3>
                                <img src="${thumbnail}">
                                <button class="select-button" data-video-id="${videoId}">Select</button>
                              </div>
                            `;
-            $("#results").append(html);
-          }
-        });
-      });
+                  $("#results").append(html);
+                }
+              });
+            });
 
-      // 選択ボタンがクリックされたら、その動画の埋め込みコードを生成して表示する
-      $(document).on("click", ".select-button", function () {
-        // データ属性から動画のIDを取得する
-        var videoId = $(this).data("video-id");
-        //var data_url = new XMLHttpRequest();
-
-       // data_url.open('GET', "///c%3A/Users/086-g7710309-72/Downloads/presentation.html");
-        //data_url.send();
-
-        //data_url.onreadystatechange = function () {
-        //  if (data_url.readyState === 4 && data_url.status === 200) {
-        //    (data_url.responseText);
-        //  }
-        //}
-        // 埋め込みコードを生成する
-        var embedCode = `
-                              <div class="box2">
-                                <h2>${query}</h2>
-                                <iframe width="600" height="400" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-
-                                </div>
-                                                       `;
-        // 埋め込みコードをアラートで表示する（Webアプリケーションに応じて変更してください）
-        //shell実装
-        alert(`
-        受理しました。
-        動画の表示ができていなくても、こちらで修正します。
-        `);
-        $("#pin").append(embedCode);
-
-
-        
-      });
-    </script>
-  </div>
+            // 選択ボタンがクリックされたら、その動画の埋め込みコードを生成して表示する
+            $(document).on("click", ".select-button", function () {
+              // データ属性から動画のIDを取得する
+              var videoId = $(this).data("video-id");
+              // 埋め込みコードを生成する
+              var embedCode = `
+              <h2>${quary}</h2>
+              <div class="box2">
+                <iframe width="600" height="400" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                </div>
+                                     `;
+              // 埋め込みコードをアラートで表示する（Webアプリケーションに応じて変更してください）
+              pin.insertAdjacentHTML('beforeend', embedCode);
+            });
+          </script>
+      </div>
 
 
 </body>
